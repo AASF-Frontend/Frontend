9d6e40f9290e409e6d2f112a200d79fc5f6c7ce5
Arjun Soni <94898141+ArjunKSoni@users.noreply.github.com>
2024-01-10 20:05:22 +0530

    dependencies

---
+++ src/Screens/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import AddAPhotoIcon from '@mui/icons-material/AddAPhoto';
+
+export default function Home() {
+  return (
+    <div className='flex-1 flex flex-col items-center justify-center'>
+      Home
+      <AddAPhotoIcon color='red' className='text-red' />
+    </div>
+  )
+}
